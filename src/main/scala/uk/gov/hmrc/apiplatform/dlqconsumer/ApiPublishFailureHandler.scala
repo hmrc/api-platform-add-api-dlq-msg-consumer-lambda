e3e4a7fef54f00a87dcd04481ed46ce27894815b
Neil Frow <675806+worthydolt@users.noreply.github.com>
2026-09-11 17:12:29 +0100

    API-8752: logging to figure out where SNS message is vanishing to

--- src/main/scala/uk/gov/hmrc/apiplatform/dlqconsumer/ApiPublishFailureHandler.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/dlqconsumer/ApiPublishFailureHandler.scala
@@ -1,15 +1,18 @@
 package uk.gov.hmrc.apiplatform.dlqconsumer
-import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import com.amazonaws.services.lambda.runtime.{Context, LambdaLogger, RequestHandler}
+import software.amazon.awssdk.utils.Logger
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class ApiPublishFailureHandler  extends RequestHandler[Object,String]{
+class ApiPublishFailureHandler  extends RequestHandler[Object,String] with Logger{
   private def getSnsService() = {
     new SnsService()
   }
 
   override def handleRequest(input: Object, context: Context): String = {
+    val log:LambdaLogger = context.getLogger
     val snsService = getSnsService()
+    log.log(s"Sending message to topic ${snsService.client.}")
     snsService.sendMessage(input.toString)
     "sent"
     }
