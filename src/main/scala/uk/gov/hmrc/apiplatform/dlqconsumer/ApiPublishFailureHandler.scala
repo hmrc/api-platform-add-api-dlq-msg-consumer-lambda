012b80b497786834d2a4791bc867146cb2254c47
Neil Frow <675806+worthydolt@users.noreply.github.com>
2026-09-23 12:18:19 +0100

    API-8752: rework without Futures and begin unit tests

--- src/main/scala/uk/gov/hmrc/apiplatform/dlqconsumer/ApiPublishFailureHandler.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/dlqconsumer/ApiPublishFailureHandler.scala
@@ -8,7 +8,7 @@ class ApiPublishFailureHandler  extends RequestHandler[Object,String] {
     new SnsService()
   }
 
-  override def handleRequest(input: Object, context: Context): String = {
+  override def handleRequest(input: Object, context: Context) = {
     Console.println(s"Entering handleRequest with message of type ${input.getClass.getName}")
     val snsService = getSnsService()
     Console.println(s"SNSService is $snsService")
