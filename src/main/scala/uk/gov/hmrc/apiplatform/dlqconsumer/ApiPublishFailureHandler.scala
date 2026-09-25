d9bdc2337f413818300c181958acdaf73d42e33c
Neil Frow <675806+worthydolt@users.noreply.github.com>
2026-09-25 16:04:18 +0100

    API-8752: figure out expected message format

--- src/main/scala/uk/gov/hmrc/apiplatform/dlqconsumer/ApiPublishFailureHandler.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/dlqconsumer/ApiPublishFailureHandler.scala
@@ -1,7 +1,5 @@
 package uk.gov.hmrc.apiplatform.dlqconsumer
-import com.amazonaws.services.lambda.runtime.{Context, LambdaLogger, RequestHandler}
-
-import scala.concurrent.ExecutionContext.Implicits.global
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
 class ApiPublishFailureHandler  extends RequestHandler[Object,String] {
   private def getSnsService() = {
@@ -12,7 +10,7 @@ class ApiPublishFailureHandler  extends RequestHandler[Object,String] {
     Console.println(s"Entering handleRequest with message of type ${input.getClass.getName}")
     val snsService = getSnsService()
     Console.println(s"SNSService is $snsService")
-    snsService.sendMessage(input.toString, context)
+    snsService.sendMessage("""{"message": {"key":"value", "key1":"value1"}}""", context)
     "sent"
     }
   }
